b425cb12507b605e6fb20b88926c8001c5ce67ad
Ben Weinshel <bmweinshel15@blakeschool.org>
2013-12-03 10:34:51 -0600

    changed main.py variables, added spaces in html jinja2 placeholder

--- main.py
+++ main.py
@@ -25,10 +25,12 @@ import jinja2
 jinja_environment = jinja2.Environment(autoescape=True,
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
+current_block = '2'
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template_values = {
-            'block': '2',
+            'block': current_block,
         }
 
         template = jinja_environment.get_template('frontendproto.html')
