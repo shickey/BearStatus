6aaadaf59b2069758706a7c8f7208e35c2dba116
Yona <ygcooperman15@blakeschool.org>
2013-12-08 15:19:06 -0600

    Edit page request handler

--- edit.py
+++ edit.py
@@ -1,2 +1,14 @@
 # request handler for editing page (/edit) goes here
 # assume admin login has already been handled
+
+import cgi
+from google.appengine.api import users
+import webapp2
+
+
+class Edit(webapp2.RequestHandler):
+
+
+app = webapp2.WSGIApplication([
+    ('/edit', Edit)
+], debug=True)
