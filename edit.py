20d1fa046135c859b95dda9572ff0c878905e232
Yona <ygcooperman15@blakeschool.org>
2013-12-09 12:02:52 -0600

    added template code

--- edit.py
+++ edit.py
@@ -8,6 +8,10 @@ import webapp2
 
 class Edit(webapp2.RequestHandler):
 
+template_values = {    }
+
+        template = jinja_environment.get_template('edit.html')
+        self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/edit', Edit)
