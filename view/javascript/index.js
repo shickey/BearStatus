f777fda1ce9a314a4d67d5049980640fe2219230
Ben Weinshel <bmweinshel15@blakeschool.org>
2014-04-07 15:13:12 -0500

    Some improvements to auto refresh

--- view/javascript/index.js
+++ view/javascript/index.js
@@ -1,16 +1,17 @@
 function refreshAt(hours, minutes, seconds) {
-    
-    // declare and set variables
-    var now = new Date();
-    var then = new Date();
-    then.setHours(hours);
-    then.setMinutes(minutes);
-    then.setSeconds(seconds);
-    
-    // if the time to refresh has passed, reload the page
-    if (then.getTime() - now.getTime() <= 0)
-    {
-        window.location.reload(true);
+    if (navigator.onLine) {  
+        // declare and set variables
+        var now = new Date();
+        var then = new Date();
+        then.setHours(hours);
+        then.setMinutes(minutes);
+        then.setSeconds(seconds);
+        
+        // if the time to refresh has passed, reload the page
+        if (then.getTime() - now.getTime() <= 0)
+        {
+            window.location.reload(true);
+        }
     }
 }
 
